b5090daab9d013ec010176c52eebeee7cd810cd9
rahulkumarparida <rahulkumarparida52@gmail.com>
2026-02-01 16:54:38 +0530

    feat: Update .gitignore to include .env.prod and modify ALLOWED_HOSTS in settings.py for production deployment

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','meeshoclone-api.onrender.com']
 
 AUTH_USER_MODEL = 'users.User'
 
