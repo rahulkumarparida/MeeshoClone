c1a9986da06a292b7f54ff0cda964a4ccab07bcc
rahulkumarparida <rahulkumarparida52@gmail.com>
2026-02-01 20:25:59 +0530

    fix: Remove Celery task call from health_check function to simplify response

--- Backend/config/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-import time
-
-@shared_task
-def health_celery():
-    time.sleep(5)
-    return "Celery is up and running safely."
--- Backend/config/views.py
+++ Backend/config/views.py
@@ -5,8 +5,7 @@ from rest_framework import status
 
 @api_view(['GET'])
 def health_check(request):
-        # task = health_celery.delay()
-        return Response({"status": "healthy and working","celery_check":task.id},status=status.HTTP_200_OK)
+        return Response({"status": "healthy and working"},status=status.HTTP_200_OK)
 
 
     
