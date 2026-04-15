5b497c1fd13ead42cc0dd1de34ac6eecdffe97f8
rahulkumarparida <rahulkumarparida52@gmail.com>
2026-04-15 10:58:07 +0530

    Final Deployement

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','0.0.0.0','10.22.212.235','meeshoclone-api.onrender.com',"meesho-clone-pink.vercel.app"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','0.0.0.0','10.22.212.235','meeshoclone-api.onrender.com',"meesho-clone-pink.vercel.app","meeshoclone-api-pxgz.onrender.com"]
 
 AUTH_USER_MODEL = 'users.User'
 
